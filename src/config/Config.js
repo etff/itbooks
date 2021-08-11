20a6f86457d34986c2d29780fd27d03340cfa933
etff <wish2rich@gmail.com>
2021-08-11 18:51:18 +0900

    feature: 네비게이션 메뉴 추가

--- src/config/Config.js
+++
@@ -1,8 +0,0 @@
-export const USER_SERVER = "/api/users";
-
-export const API_URL = "https://api.themoviedb.org/3/";
-export const API_KEY = "b18d487ce9b217d556f3e426aac32a65";
-export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
-export const BACKDROP_SIZE = "w1280";
-export const IMAGE_SIZE = "w1280";
-export const POSTER_SIZE = "w500";
--- src/index.js
+++ src/index.js
@@ -4,4 +4,26 @@ import "./index.css";
 import App from "./App";
 import "antd/dist/antd.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import Reducer from "./reducers";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+
+ReactDOM.render(
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
